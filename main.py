04e91000c70bfc67f269eb930df5b18fdaed203f
Peter Luo <zl2005.luo@icloud.com>
2020-08-22 10:25:27 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import re
+
+while True:
+    statement = input()
+    statement = re.sub(r'(人|人类)','你', statement)
+    statement = re.sub(r'(进化|变的)','生', statement)
+    statement = re.sub(r'(猴子|猩猩)','你妈', statement)
+    print(statement)
+
