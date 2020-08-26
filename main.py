0be3a132980691a1bf5aa22f553647e5b7cf5a92
Qian Yixin <qianyixin@datagrand.com>
2020-08-26 16:38:21 +0800

    feat(conf): add conf with substitution dict

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-import re
+# -*- encoding: utf-8 -*-
+import logging
+import jieba
+from conf import substitution_words, DEBUG
 
-while True:
+# global set
+jieba.setLogLevel(log_level=logging.INFO)
+for word, _ in substitution_words.items():
+    jieba.add_word(word, freq=10000)
+
+
+def substitute_statement(statement, substitution_dict):
+    """
+    replace word in input statement
+    :param str statement: input statement
+    :param dict[str, str] substitution_dict: replace key with value
+    :return: replaced statement
+    """
+    statement_words = jieba.lcut(statement)
+    if DEBUG:
+        print(list(statement_words))
+    substituted_words = [substitution_dict.get(word, word) for word in statement_words]
+    return ''.join(substituted_words)
+
+
+def main():
     statement = input()
-    statement = re.sub(r'(人类|人)','你', statement)
-    statement = re.sub(r'(进化|变|变的)','生', statement)
-    statement = re.sub(r'(猴子|猩猩)','你妈', statement)
+    statement = substitute_statement(statement, substitution_words)
     print(statement)
 
+
+if __name__ == "__main__":
+    while True:
+        main()
